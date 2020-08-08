330a9553bb121cf0d4bbc052d4203d56b0fa1700
boom786 <66739388+boom786@users.noreply.github.com>
2020-08-08 10:36:18 +0530

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		string s;
+		cin >> s;
+		
+		stack<char> seq;
+		
+		int n = s.size() , len = 0;
+		bool flag = true;
+		for(int i = 0 ; i < n ; i++){
+			if(s[i] == '(' || s[i] == '{' || s[i] == '['){ 
+				seq.push(s[i]);		//if opening backet, push on stack
+				len++;
+			}
+			else{	// check the corresponding opening bracket, and pop 
+				if(s[i] == ')' && len && seq.top() == '('){
+					seq.pop();
+					len--;
+				}
+				else if(s[i] == '}' && len && seq.top() == '{'){
+					seq.pop();
+					len--;
+				}
+				else if(s[i] == ']' && len && seq.top() == '['){
+					seq.pop();
+					len--;
+				}
+				else{	// if no corresponding opening bracket is tere then the sequence is not balanced
+					flag = false;
+					break;
+				}
+			}
+		}
+		
+		if(len)		// if at the end the stack is not empty, sequence is unbalanced
+			flag = false;
+		
+		if(flag)	// seq balanced
+			cout << "YES" << endl;
+		else        // seq unbalanced
+			cout << "NO" << endl;
+	}
+}
